97c75d0cfada5601796a6a4996a2d44109958906
Dmitrii Mamonov <dmitry.mamonov@justwebit.ru>
2017-04-24 20:27:12 +0300

    add script for color article's borders

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,19 @@ function activateButtons(e) {
   document.getElementById('buttons-main').classList.toggle('page-header__buttons-main--active');
   document.getElementById('main-nav').classList.toggle('main-nav--active');
 }
+
+var articles = document.querySelectorAll('.article');
+var articlesColors = [
+	'#08b0e0',
+	'#ef5c5c',
+	'#3360b2',
+	'#2eb398',
+	'#ae0a0a',
+	'#5692b1'
+];
+
+window.onload = function() {
+ 	for (var i = 0; i < articles.length; i++) {
+	  articles[i].style.borderBottomColor = articlesColors[i];
+	}
+}
